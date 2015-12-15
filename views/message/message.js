a51d7092a407568d67dddc4767bdb53293e04c2e
Eamon <Chenlulei@live.com>
2015-12-15 21:40:41 +0800

    add chat page for testing.

--- views/message/message.js
+++ views/message/message.js
@@ -4,20 +4,41 @@
 
 var app= angular.module("app_message",[]);
 
-app.controller('messageSeverControl', function ($scope) {
+app.controller('messageControl', function ($scope) {
 
     $scope.username = 'E';
     $scope.show_page = 'login';
-
-    $scope.onEnter = function(e){
+    $scope.msglist = [];
+    //var chatusername;
+    $scope.onEnter = function(e,name){
 
         var key = window.event?e.keyCode:e.which;
         if(key==13){
-            $scope.username =  $scope.username.toString() + ' chen';
-            alert('OK');
+            $scope.username = name + ' chen';
+            //alert('OK');
+            $scope.msglist.push($scope.username +' : come in.');
             $scope.show_page = 'chat';
         }
     }
+
+    $scope.onSend = function(e,msg){
+
+        var key = window.event?e.keyCode:e.which;
+        if(key==13){
+            var item = $scope.username +' : '+msg;
+            $scope.msglist.push(item);
+        }
+    }
+
+    var socket = io.connect('http://192.168.2.241:3000');
+
+    socket.on('connected',function(socket){
+        console.log('connected to Server!');
+        socket.on('chat message', function(msg){
+            console.log('message: ' + msg);
+        });
+    });
+
 
 });
 
