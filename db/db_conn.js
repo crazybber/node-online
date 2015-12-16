c4377ff4f0fa3e6b397d8842a271b8dc8c6b4a7f
Eamon <Chenlulei@live.com>
2015-12-16 16:22:25 +0800

    add readme and adjust js location dir

--- db/db_conn.js
+++ db/db_conn.js
@@ -8,9 +8,11 @@ var mongoose= require('mongoose');
 
 mongoose.connect('mongodb://localhost/TestDB');
 
-
+//db.on('error', console.error.bind(console, 'connection error:'));
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', function (err) {
+    console.error.bind(console, err+ 'connection error:');
+});
 db.once('open', function (callback) {
     // success open db conn!
 });
--- routes/admin/resourceMgr.js
+++ routes/admin/resourceMgr.js
@@ -1,3 +1,4 @@
 /**
  * Created by Administrator on 2015/12/16.
+ *
  */
