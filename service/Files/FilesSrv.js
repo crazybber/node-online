295ef1c04073e255ea1d9a7f8636f3d88db206ec
Eamon <Chenlulei@live.com>
2015-12-17 00:45:06 +0800

    add filetype maping for download

--- service/Files/FilesSrv.js
+++ service/Files/FilesSrv.js
@@ -1,4 +1,5 @@
 /**
  * Created by Administrator on 2015/12/16.
- * FileServer to Store MicroService Module.
+ * FileServer to Store MicroService Module.文件下载服务，下载.apk插件给客户端。
  */
+
---
+++ service/Files/uploadfiles.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2015/12/16.
+ * upload files 上传文件
+ */
