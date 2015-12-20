8fb5d016af016ff7d5d2f90694b2254fa07c9c96
Eamon <Chenlulei@live.com>
2015-12-20 15:54:06 +0800

    add user table modules design

---
+++ db/user_mapping.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2015/12/20.
+ * 保存第三方的登陆账户与本地账户的映射信息
+ * 比如第三方：userID：asdad-2323adad-adsad-hggh--2334b映射一个到本地：99werw-sfsdjkfsd-sasdd-bmmn53
+ *
+ */
---
+++ db/user_status.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2015/12/20.
+ * 用户当前状态，是否在在线，登陆区域，登陆终端，当前持有的token。
+ */
