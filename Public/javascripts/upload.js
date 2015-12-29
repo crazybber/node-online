5de289e0c7ff9452e02b190c38ea0ef0061c42c2
Eamon <Chenlulei@live.com>
2015-12-29 16:17:54 +0800

    update codes for update function

--- Public/javascripts/upload.js
+++ Public/javascripts/upload.js
@@ -9,40 +9,14 @@ app.controller("uploadCtrl",function($scope,$http){
 
     var url="http://127.0.0.1:3000";
 
-    var path ='111122';
+    var path ='test path ';
     var data=  {
         name:path,
-        other:'others'
+        other:'others',
+        file:$scope.filename
     };
-
-    $scope.testfunction =function() {
-
-        var config = {
-            headers: {'Content-Type': undefined}
-        };
-
-        $http.post(url + '/upload', data, config)
-            .success(function (reg_return) {
-
-            var result = reg_return.reg_result;
-            if (result.result === true) {
-
-            }
-
-        }).error(function(data, status, headers, config) {
-            console.log(data);
-            console.log(status);
-            console.log(headers);
-            console.log(config);
-
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
-    };
-
 
     $scope.goSubmit =function(){
-
         var config = {
             headers : { 'Content-Type': undefined }
         };
