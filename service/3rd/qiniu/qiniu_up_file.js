e16736197bc772a806bf5c82abef30504c0ce755
XinXinSi <1214957011@qq.com>
2016-02-22 19:37:04 +0800

    1.修改七牛上传文件部分：修改上传uptoken的生成

--- service/3rd/qiniu/qiniu_up_file.js
+++ service/3rd/qiniu/qiniu_up_file.js
@@ -6,11 +6,9 @@ var qiniu = require('qiniu');
 var uptoken = require('./uptoken.js');
 
 exports.uploadFile = function () {
-    var extra = new qiniu.io.PutExtra()
-    extra.mimeType = 'file';
-    var key = 'ww.png';
+    var key = 'ww';
     var localFile = './bb.png'
-    qiniu.io.putFile(uptoken, key, localFile, extra, function (err, res) {
+    qiniu.io.putFile(uptoken.uptoken, key, localFile, uptoken.extra, function (err, res) {
         if (!err) {       //上传成功,处理返回值
             console.log(res.key, res.hash);
         } else {          //下传失败，处理返回值
