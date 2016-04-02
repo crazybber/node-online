54eb42a79a493a069541435c81dee80556671356
Eamon <Chenlulei@live.com>
2016-04-02 19:15:58 +0800

    check logic code for reg

--- db/dao/auth/users.js
+++ db/dao/auth/users.js
@@ -14,6 +14,8 @@ function check_reg(params)
 //校验参数中的两个字段，并返回结果
     console.log('check_reg');
 
+ //从数据库中查询出不能注册的条件，检查当前注册行为里的信息，是否需要被过滤掉。
+//TODO ...次调试进行到这里
     reg_check.find({can_register:false},function(err,reg_checks){
 
         //TODO... check logic code for condition.
